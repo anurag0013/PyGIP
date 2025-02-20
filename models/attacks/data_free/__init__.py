17a720fb8e70a9c58fc2b9b68c7cca229f676249
Kaixiang Zhao <kxzhao2002@gmail.com>
2025-02-19 19:19:29 -0500

    Update __init__.py

--- models/attacks/data_free/__init__.py
+++ models/attacks/data_free/__init__.py
@@ -2,10 +2,14 @@ from .base import DataFreeBaseAttack
 from .type_i import TypeIAttack
 from .type_ii import TypeIIAttack
 from .type_iii import TypeIIIAttack
+from .generator import GraphGenerator
+from .surrogate import SurrogateModel
 
 __all__ = [
     'DataFreeBaseAttack',
     'TypeIAttack',
     'TypeIIAttack',
-    'TypeIIIAttack'
+    'TypeIIIAttack',
+    'GraphGenerator',
+    'SurrogateModel'
 ]
