ae30bed0fa7c5a81a100f51f5282122fcccf1053
Kaixiang Zhao <kxzhao2002@gmail.com>
2025-02-19 19:11:23 -0500

    Create __init__.py

---
+++ models/attacks/data_free/__init__.py
@@ -0,0 +1,11 @@
+from .base import DataFreeBaseAttack
+from .type_i import TypeIAttack
+from .type_ii import TypeIIAttack
+from .type_iii import TypeIIIAttack
+
+__all__ = [
+    'DataFreeBaseAttack',
+    'TypeIAttack',
+    'TypeIIAttack',
+    'TypeIIIAttack'
+]
